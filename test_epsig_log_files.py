9df91018def538734795e0c380380b02763f2af9
icarus523 <icarus523@users.noreply.github.com>
2018-07-27 10:08:17 +1000

    Changes 27/7/2018
    Verifies the last 4 entries in the epsig log file PSL version has been incremented by 1
    In addition to: 
    - Valid expected epsig version
    - the date start and finish time is within 7 days from current date
    - log files's footer ends "with EXIT_SUCCESS"
    
    - warns the user if entry in log file indicates it has not finished
    Verifies the last 2 entries in the epsig log file, that the command string used is correct (wrt MSL, PSL, TSL files), this was changed from only verifying the last entry.

--- test_epsig_log_files.py
+++ test_epsig_log_files.py
@@ -13,23 +13,49 @@ class EpsigLogFile():
 
     def __init__(self, data): 
         self.header = data[0] 
-        self.version_number = self.header.split(' ')[2]
-        
+        self.version_number = self.header.split(' ')[2] # 3.5
+
         self.time_stamp_start_str = data[1]
-        self.command = data[2]
+        self.command_str = data[2]
         self.allocating_buffer = data[3]
         self.time_stamp_end = data[4]
         self.footer_status = data[5]                
-
+        
         assert(len(data) == 6) # this should be 5
 
+    def is_complete(self): 
+        if self.time_stamp_start_str.strip() and self.time_stamp_end.strip() and \
+            self.footer_status == " with EXIT_SUCCESS": 
+            
+            return True
+        else:
+            return False
+    
+    def toString(self): 
+        self.epsig_log_entry_str = "\n\n%(header)s\n%(time_stamp_start_str)s\n%(command_str)s\n%(allocating_buffer)s\n%(time_stamp_end)s\n%(footer_status)s" % \
+            {   'header': self.header, 
+                'version_number': str(self.version_number), 
+                'time_stamp_start_str': self.time_stamp_start_str, 
+                'command_str': self.command_str, 
+                'allocating_buffer': self.allocating_buffer, 
+                'time_stamp_end': self.time_stamp_end, 
+                'footer_status' : self.footer_status
+            }
+
+        return self.epsig_log_entry_str.strip()
+        
 class test_epsig_log_files(QCASTestClient):
+
+    def get_psl_file_from_cmd_str(self, s): 
+        psl_file = s.split(' ')[5]
+        self.assertTrue(psl_file.endswith(".psl")) 
+        return psl_file
 
     def test_Read_Epsig_log_file_from_disk(self):
         self.assertTrue(os.path.isfile(self.my_preferences.epsig_log_file), 
         	msg=self.my_preferences.epsig_log_file + ": File not found")
 
-    def test_Epsig_Log_file(self): 
+    def test_epsig_log_file_last_four_entries_are_valid_for_psl_versions(self): 
         # EPSIG.EXE Version 3.5 Copyright The State of Queensland 1999-2015
         # Started at   Fri Oct 06 08:41:37 2017
         # D:\OLGR-TECHSERV\MISC\BINIMAGE\qcas\epsigQCAS3_5.exe d:\OLGR-TECHSERV\BINIMAGE\*.* qcas_2017_11_v01.msl qcas_2017_10_v02.tsl qcas_2017_11_v02.psl 
@@ -42,36 +68,82 @@ class test_epsig_log_files(QCASTestClient):
         with open(self.my_preferences.epsig_log_file, 'r') as file: 
             epsig_log = file.read()
             paragraphs = epsig_log.split('\n\n')
-        
-            data = paragraphs[len(paragraphs)-1].split('\n') # get last paragraph from list. 
-            data = list(filter(None, data)) # remove empty lists            
-            log_file = EpsigLogFile(data)
             
-            # Verify EPSIG version number. 
-            self.assertEqual(log_file.version_number, VALID_EPSIG_VERSION, 
-            	msg="EPSIG version not equal to: " + VALID_EPSIG_VERSION)
+            log_blocks = list() 
             
-            # The dates and start and finish time of processing appear reasonable
-            # Valid Start Date: Assume within 7 days from current date
-            time_stamp_start_obj = datetime.strptime(log_file.time_stamp_start_str[13:], "%a %b %d %H:%M:%S %Y")
-            time_delta = today - time_stamp_start_obj
-            #print("Number of days since last epsig log started: " + str(time_delta.days))
-            self.assertTrue(time_delta.days < NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG, 
-            	msg="Number of days since last epsig log started is greater than " + str(NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG)) # less than NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG days
+            # Get Last 4 paragraphs (end of file) with EXIT_SUCCESS
+            for i in list(range(1,5)): 
+                data = paragraphs[len(paragraphs)-i].split('\n') 
+                data = list(filter(None, data)) # remove empty lists    
+                log_entry = EpsigLogFile(data)
+                log_blocks.append(log_entry)
+                
+            # data = paragraphs[len(paragraphs)-1].split('\n') # get last paragraph from list. 
 
-            if len(data) > 4: 
-                time_stamp_end_obj = datetime.strptime(log_file.time_stamp_end[13:], "%a %b %d %H:%M:%S %Y")
-                time_delta = today - time_stamp_end_obj
-                #print("Number of days since last epsig log ended: " + str(time_delta.days))
+            #data = list(filter(None, data)) # remove empty lists            
+            #log_file = EpsigLogFile(data)
+            current_month_list = list() 
+            next_month_list = list() 
+            
+            for log_file in log_blocks:
+                # Verify EPSIG version number. 
+                self.assertEqual(log_file.version_number, VALID_EPSIG_VERSION, 
+                    msg="EPSIG version not equal to: " + VALID_EPSIG_VERSION)
+                
+                # The dates and start and finish time of processing appear reasonable
+                # Valid Start Date: Assume within 7 days from current date
+                time_stamp_start_obj = datetime.strptime(log_file.time_stamp_start_str[13:], "%a %b %d %H:%M:%S %Y")
+                time_delta = today - time_stamp_start_obj
+                #print("Number of days since last epsig log started: " + str(time_delta.days))
                 self.assertTrue(time_delta.days < NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG, 
-                	msg="Number of days since last epsig log started is greater than " + str(NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG)) # less than NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG days
+                    msg="Number of days since last epsig log started is greater than " + str(NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG)) # less than NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG days
+
+                if log_file.is_complete(): 
+                    time_stamp_end_obj = datetime.strptime(log_file.time_stamp_end[13:], "%a %b %d %H:%M:%S %Y")
+                    time_delta = today - time_stamp_end_obj
+
+                    self.assertTrue(time_delta.days < NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG, 
+                        msg="Number of days since last epsig log started is greater than " + str(NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG)) # less than NUMBER_OF_VALID_DAYS_SINCE_START_OF_LOG days
+                    
+                    # The latest run Epsig error file is correct.(Ref WI01)      
+                    self.assertEqual(log_file.footer_status, " with EXIT_SUCCESS", 
+                        msg="Epsig Log file did not end with 'EXIT_SUCCESS")
+                else: 
+                    print("#### WARNING: Entry in EPSIG log indicates it has not finished. ####")
+
+                # Need to Verify PSL files used in the last 4 Entries are correct
+                psl_files = list()
+                psl_files.append(self.get_psl_file_from_cmd_str(log_file.command_str))
+                for psl in psl_files: 
+                    if len(current_month_list) == 0:  
+                        current_month_list.append(psl) 
+                    else: 
+                        if current_month_list[0][10:12] == psl[10:12]: # month fields is the same
+                            current_month_list.append(psl) 
+                        else:
+                            next_month_list.append(psl)
+            
+            # Sort to make sure that filenames are in order
+            current_month_list.sort()
+            next_month_list.sort()
+            
+            self.assertTrue(int(current_month_list[1][14:16]) == (int(current_month_list[0][14:16]) + 1), "Second PSL version is not incremented by 1")
+            self.assertTrue(int(next_month_list[1][14:16]) == (int(next_month_list[0][14:16]) + 1), "Second PSL version is not incremented by 1")
+    
+    def test_epsig_log_file_last_two_entries_command_str_is_valid(self): 
+        with open(self.my_preferences.epsig_log_file, 'r') as file: 
+            epsig_log = file.read()
+            paragraphs = epsig_log.split('\n\n')
+
+            last_log_entry = paragraphs[len(paragraphs)-1].split('\n')              # get last paragraph from list. 
+            second_last_log_entry = paragraphs[len(paragraphs)-2].split('\n')       # 2nd last
+            
+            logs = [last_log_entry, second_last_log_entry]
+            for log in logs: 
+                data = list(filter(None, log)) # remove empty lists    
+                epsig_entry = EpsigLogFile(data)
                 
-                # The latest run Epsig error file is correct.(Ref WI01)      
-                self.assertEqual(log_file.footer_status, " with EXIT_SUCCESS", 
-                	msg="Epsig Log file did not end with 'EXIT_SUCCESS")
-            else: 
-                print("#### WARNING: Last entry in EPSIG log indicates it has not finished. ####")
+                self.assertTrue(epsig_entry.is_complete())
                 
-            # Ensure that the parameters of the epsig.exe call appear correct, ...         
-            self.verify_epsig_command_used(log_file.command)
-        
+                # Ensure that the parameters of the epsig.exe call appear correct, ...         
+                self.verify_epsig_command_used(epsig_entry.command_str)
